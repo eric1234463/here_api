2a87279e75e15755b110d2920558978b27ee7aae
eric1234463 <eric1234463@gmail.com>
2018-01-22 19:35:11 +0800

    add run build

--- dist/apis/patient/getBiologicalClock.js
+++ dist/apis/patient/getBiologicalClock.js
@@ -33,7 +33,7 @@ function getBiologicalClock(req, res, next) {
             if (collectionElement.type == 'SLEEP') {
                 var time = (0, _moment2.default)(collectionElement.createdAt);
                 var remainder = 30 - time.minute() % 30;
-                var roundUpTime = (0, _moment2.default)(time).add("minutes", remainder);
+                var roundUpTime = (0, _moment2.default)(time).add("minutes", remainder).hours();
                 arr.push(roundUpTime);
             }
             return arr;
@@ -42,7 +42,7 @@ function getBiologicalClock(req, res, next) {
             if (collectionElement.type == 'WAKE') {
                 var time = (0, _moment2.default)(collectionElement.createdAt);
                 var remainder = 30 - time.minute() % 30;
-                var roundUpTime = (0, _moment2.default)(time).add("minutes", remainder);
+                var roundUpTime = (0, _moment2.default)(time).add("minutes", remainder).hours();;
                 arr.push(roundUpTime);
             }
             return arr;
