f58590cd331d1780e8485941e3563bdde746ab90
eric1234463 <eric1234463@gmail.com>
2018-01-22 13:58:09 +0800

    revamp post api

--- src/apis/patient/createBiologicalClock.js
+++ src/apis/patient/createBiologicalClock.js
@@ -4,14 +4,8 @@ import models from '../../../models';
 export default function createBiologicalClock(req, res, next) {
     models.patientBiologicalClock
         .create({
-            where: {
-                type: req.body.type,
-                patientId: req.body.patientId,
-            },
-            defaults: {
-                patientId: req.body.patientId,
-                type: req.body.type,
-            }
+            type: req.body.type,
+            patientId: req.body.patientId,
         }).spread((date, created) => {
             res.json(date);
         });
