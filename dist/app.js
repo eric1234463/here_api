992776e0521bdcc41a82b30936890b61c5ec9e94
eric1234463 <eric1234463@gmail.com>
2018-01-23 18:10:10 +0800

    refactor http call

--- dist/app.js
+++ dist/app.js
@@ -39,15 +39,17 @@ app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 
 app.options("/*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', true);
   res.send(200);
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
   if ('OPTIONS' === req.method) {
     //respond with 200
     res.send(200);
