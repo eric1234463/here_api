50d6a416f260d3522516d02813c71a3a3d20857b
eric1234463 <eric1234463@gmail.com>
2018-01-29 12:02:46 +0800

    change cors

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-var _express = require('express');
+var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
-var _bodyParser = require('body-parser');
+var _bodyParser = require("body-parser");
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
-var _cors = require('cors');
+var _cors = require("cors");
 
 var _cors2 = _interopRequireDefault(_cors);
 
-var _http = require('http');
+var _http = require("http");
 
 var _http2 = _interopRequireDefault(_http);
 
-var _socket = require('socket.io');
+var _socket = require("socket.io");
 
 var _socket2 = _interopRequireDefault(_socket);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var apiHandlers = require('./apis');
+var apiHandlers = require("./apis");
 var app = (0, _express2.default)();
 var server = _http2.default.Server(app);
 var io = new _socket2.default(server);
@@ -31,7 +31,7 @@ var port = process.env.PORT || 8080;
 
 // view engine setup
 app.use((0, _cors2.default)());
-app.set('view engine', 'html');
+app.set("view engine", "html");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -39,18 +39,18 @@ app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 
 app.options("/*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", true);
   res.send(200);
 });
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', true);
-  if ('OPTIONS' === req.method) {
+  res.header("Access-Control-Allow-Credentials", true);
+  if ("OPTIONS" === req.method) {
     //respond with 200
     res.send(200);
   } else {
@@ -59,10 +59,10 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use('/api', apiHandlers);
+app.use("/api", apiHandlers);
 
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -71,34 +71,34 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-io.on('connection', function (socket) {
-  socket.on('subscribe', function (room) {
-    console.log('joining room', room);
+io.on("connection", function (socket) {
+  socket.on("subscribe", function (room) {
+    console.log("joining room", room);
     socket.join(room);
   });
 
-  socket.on('connect doctor', function (data) {
-    socket.broadcast.to(data.room).emit('connect doctor', {
+  socket.on("connect doctor", function (data) {
+    socket.broadcast.to(data.room).emit("connect doctor", {
       patient: data.patient
     });
   });
-  socket.on('cancel connection', function (data) {
-    console.log('cancel connection', data);
-    socket.broadcast.to(data.room).emit('cancel doctor', {
+  socket.on("cancel connection", function (data) {
+    console.log("cancel connection", data);
+    socket.broadcast.to(data.room).emit("cancel doctor", {
       patient: data.patient
     });
   });
 });
 
 server.listen(port, function () {
-  console.log('listening on *:' + port);
+  console.log("listening on *:" + port);
 });
 module.exports = { app: app, server: server };
 //# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import http from 'http';
-import SocketIO from 'socket.io';
-let apiHandlers = require('./apis');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import http from "http";
+import SocketIO from "socket.io";
+let apiHandlers = require("./apis");
 let app = express();
 let server = http.Server(app);
 let io = new SocketIO(server);
@@ -12,78 +12,79 @@ let port = process.env.PORT || 8080;
 
 // view engine setup
 app.use(cors());
-app.set('view engine', 'html');
+app.set("view engine", "html");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.options("/*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', true);
+app.options("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
   res.send(200);
 });
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', true);
-  if ('OPTIONS' === req.method) {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  if ("OPTIONS" === req.method) {
     //respond with 200
     res.send(200);
-  }
-  else {
+  } else {
     //move on
     next();
   }
-
 });
 
-app.use('/api', apiHandlers);
+app.use("/api", apiHandlers);
 
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
-io.on('connection', (socket) => {
-  socket.on('subscribe', function (room) {
-    console.log('joining room', room);
+io.on("connection", socket => {
+  socket.on("subscribe", function(room) {
+    console.log("joining room", room);
     socket.join(room);
   });
 
-  socket.on('connect doctor', function (data) {
-    socket.broadcast.to(data.room).emit('connect doctor', {
+  socket.on("connect doctor", function(data) {
+    socket.broadcast.to(data.room).emit("connect doctor", {
       patient: data.patient
     });
   });
-  socket.on('cancel connection', function (data) {
-    console.log('cancel connection', data);
-    socket.broadcast.to(data.room).emit('cancel doctor', {
+  socket.on("cancel connection", function(data) {
+    console.log("cancel connection", data);
+    socket.broadcast.to(data.room).emit("cancel doctor", {
       patient: data.patient
     });
   });
 });
 
-server.listen(port, function () {
-  console.log('listening on *:' + port);
+server.listen(port, function() {
+  console.log("listening on *:" + port);
 });
 module.exports = { app: app, server: server };
