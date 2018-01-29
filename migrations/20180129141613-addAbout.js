4753fafcaec09b56135ad1f22a5a6634550feafb
eric1234463 <eric1234463@gmail.com>
2018-01-29 22:20:16 +0800

    add about

---
+++ migrations/20180129141613-addAbout.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return [
+      queryInterface.addColumn("doctor", "about", Sequelize.STRING, {
+        after: "photoURL"
+      }),
+      queryInterface.addColumn("doctor", "telphone", Sequelize.STRING, {
+        after: "photoURL"
+      }),
+      queryInterface.addColumn("doctor", "age", Sequelize.STRING, {
+        after: "photoURL"
+      })
+    ];
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return [
+      queryInterface.removeColumn("doctor", "telphone"),
+      queryInterface.removeColumn("doctor", "about"),
+      queryInterface.removeColumn("doctor", "age")
+    ];
+  }
+};
--- models/doctor.js
+++ models/doctor.js
@@ -1,21 +1,24 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-    var Doctor = sequelize.define(
-        'Doctor',
-        {
-            id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
-            uid: { type: DataTypes.STRING, allowNull: true },
-            email: { type: DataTypes.STRING, allowNull: true },
-            displayName: { type: DataTypes.STRING, allowNull: true },
-            photoURL: { type: DataTypes.STRING, allowNull: true },
-            location: { type: DataTypes.STRING, allowNull: true },
-            google_lat: { type: DataTypes.DECIMAL, allowNull: true },
-            google_lng: { type: DataTypes.DECIMAL, allowNull: true },
-        },
-        {
-            tableName: 'doctor'
-        }
-    );
-    Doctor.associate = function (models) { };
-    return Doctor;
+  var Doctor = sequelize.define(
+    "Doctor",
+    {
+      id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
+      uid: { type: DataTypes.STRING, allowNull: true },
+      email: { type: DataTypes.STRING, allowNull: true },
+      displayName: { type: DataTypes.STRING, allowNull: true },
+      photoURL: { type: DataTypes.STRING, allowNull: true },
+      location: { type: DataTypes.STRING, allowNull: true },
+      about: { type: DataTypes.STRING, allowNull: true },
+      telphone: { type: DataTypes.STRING, allowNull: true },
+      gender: { type: DataTypes.STRING, allowNull: true },
+      google_lat: { type: DataTypes.DECIMAL, allowNull: true },
+      google_lng: { type: DataTypes.DECIMAL, allowNull: true }
+    },
+    {
+      tableName: "doctor"
+    }
+  );
+  Doctor.associate = function(models) {};
+  return Doctor;
 };
