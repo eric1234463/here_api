a57e07d39ff4120aea6c9cc7f52f536a319ef45a
eric1234463 <eric1234463@gmail.com>
2018-02-03 16:13:25 +0800

    update createWalkStep

--- dist/apis/patient/createWalkingStep.js
+++ dist/apis/patient/createWalkingStep.js
@@ -12,16 +12,24 @@ var _models2 = _interopRequireDefault(_models);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function createWalkingStep(req, res, next) {
-  _models2.default.patientWalkingStep.findOrCreate({
-    defaults: {
-      value: req.body.step
-    },
+  var value = {
+    patientId: req.body.patientId,
+    date: req.body.date,
+    value: req.body.step
+  };
+  _models2.default.patientWalkingStep.findOne({
     where: {
       patientId: req.body.patientId,
       date: req.body.date
     }
-  }).spread(function (clock, created) {
-    res.json({ status: created });
+  }).then(function (obj) {
+    if (obj) {
+      // update
+      return obj.update(values);
+    } else {
+      // insert
+      return Model.create(values);
+    }
   });
 }
 //# sourceMappingURL=createWalkingStep.js.map
--- src/apis/patient/createWalkingStep.js
+++ src/apis/patient/createWalkingStep.js
@@ -1,17 +1,25 @@
 import models from "../../../models";
 
 export default function createWalkingStep(req, res, next) {
+  let value = {
+    patientId: req.body.patientId,
+    date: req.body.date,
+    value: req.body.step
+  };
   models.patientWalkingStep
-    .findOrCreate({
-      defaults: {
-        value: req.body.step
-      },
+    .findOne({
       where: {
         patientId: req.body.patientId,
         date: req.body.date
       }
     })
-    .spread((clock, created) => {
-      res.json({ status: created });
+    .then(function(obj) {
+      if (obj) {
+        // update
+        return obj.update(values);
+      } else {
+        // insert
+        return Model.create(values);
+      }
     });
 }
