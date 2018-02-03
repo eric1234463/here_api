4ea98f42aff29e5d067f790c22eb1c2bc8b7bdf3
eric1234463 <eric1234463@gmail.com>
2018-02-03 15:49:53 +0800

    add walking step

--- dist/apis/patient/router.js
+++ dist/apis/patient/router.js
@@ -1,33 +1,38 @@
-'use strict';
+"use strict";
 
-var _express = require('express');
+var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
-var _patientLogin = require('./patientLogin');
+var _patientLogin = require("./patientLogin");
 
 var _patientLogin2 = _interopRequireDefault(_patientLogin);
 
-var _createBiologicalClock = require('./createBiologicalClock');
+var _createBiologicalClock = require("./createBiologicalClock");
 
 var _createBiologicalClock2 = _interopRequireDefault(_createBiologicalClock);
 
-var _getBiologicalClock = require('./getBiologicalClock');
+var _getBiologicalClock = require("./getBiologicalClock");
 
 var _getBiologicalClock2 = _interopRequireDefault(_getBiologicalClock);
 
-var _updateProfile = require('./updateProfile');
+var _updateProfile = require("./updateProfile");
 
 var _updateProfile2 = _interopRequireDefault(_updateProfile);
+
+var _createWalkingStep = require("./createWalkingStep");
+
+var _createWalkingStep2 = _interopRequireDefault(_createWalkingStep);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var patientRouter = _express2.default.Router();
 
-patientRouter.put('/', _updateProfile2.default);
-patientRouter.post('/login', _patientLogin2.default);
-patientRouter.post('/biologicalClock', _createBiologicalClock2.default);
-patientRouter.get('/biologicalClock', _getBiologicalClock2.default);
+patientRouter.put("/", _updateProfile2.default);
+patientRouter.post("/login", _patientLogin2.default);
+patientRouter.post("/biologicalClock", _createBiologicalClock2.default);
+patientRouter.post("/WalkingStep", _createWalkingStep2.default);
+patientRouter.get("/biologicalClock", _getBiologicalClock2.default);
 
 module.exports = patientRouter;
 //# sourceMappingURL=router.js.map
--- src/apis/patient/router.js
+++ src/apis/patient/router.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import patientLogin from './patientLogin';
-import createBiologicalClock from './createBiologicalClock';
-import getBiologicalClock from './getBiologicalClock';
-import updateProfile from './updateProfile';
+import express from "express";
+import patientLogin from "./patientLogin";
+import createBiologicalClock from "./createBiologicalClock";
+import getBiologicalClock from "./getBiologicalClock";
+import updateProfile from "./updateProfile";
+import createWalkingStep from "./createWalkingStep";
 
 const patientRouter = express.Router();
 
-patientRouter.put('/', updateProfile);
-patientRouter.post('/login', patientLogin);
-patientRouter.post('/biologicalClock', createBiologicalClock);
-patientRouter.get('/biologicalClock', getBiologicalClock);
+patientRouter.put("/", updateProfile);
+patientRouter.post("/login", patientLogin);
+patientRouter.post("/biologicalClock", createBiologicalClock);
+patientRouter.post("/WalkingStep", createWalkingStep);
+patientRouter.get("/biologicalClock", getBiologicalClock);
 
 module.exports = patientRouter;
