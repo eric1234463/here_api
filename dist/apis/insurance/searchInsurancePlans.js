6d19ca99a722916b6d1018289e2d09d090131a0e
eric1234463 <eric1234463@gmail.com>
2018-03-06 20:43:25 +0800

    add assign attr to test

--- dist/apis/insurance/searchInsurancePlans.js
+++ dist/apis/insurance/searchInsurancePlans.js
@@ -14,23 +14,24 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function searchInsurancePlans(req, res, next) {
   console.log(req.body);
   var condtion = {
-    order: [["id", "ASC"]]
+    order: [["id", "ASC"]],
+    where: {}
   };
 
   if (!req.body.search.provider) {
-    condtion.where.provider = {
+    condtion['where']['provider'] = {
       $eq: req.body.search.provider
     };
   }
 
   if (!req.body.search.surgery_cover) {
-    condtion.where.surgery_cover = {
+    condtion['where']['surgery_cover'] = {
       $gte: req.body.surgery_cover
     };
   }
 
   if (!req.body.search.daliy_cover) {
-    condtion.where.daliy_cover = {
+    condtion['where']['daliy_cover'] = {
       $gte: req.body.search.daliy_cover
     };
   }
--- src/apis/insurance/searchInsurancePlans.js
+++ src/apis/insurance/searchInsurancePlans.js
@@ -3,23 +3,24 @@ import models from "../../../models";
 export default function searchInsurancePlans(req, res, next) {
   console.log(req.body);
   let condtion = {
-    order: [["id", "ASC"]]
+    order: [["id", "ASC"]],
+    where:{}
   };
 
   if (!req.body.search.provider) {
-    condtion.where.provider = {
+    condtion['where']['provider'] = {
       $eq: req.body.search.provider
     };
   }
 
   if (!req.body.search.surgery_cover) {
-    condtion.where.surgery_cover = {
+    condtion['where']['surgery_cover'] = {
       $gte: req.body.surgery_cover
     };
   }
 
   if (!req.body.search.daliy_cover) {
-    condtion.where.daliy_cover = {
+    condtion['where']['daliy_cover'] = {
       $gte: req.body.search.daliy_cover
     };
   }
