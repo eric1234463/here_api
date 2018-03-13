da1361a1f2325f11aed4ff1df2ff3749c6c80376
eric1234463 <eric1234463@gmail.com>
2018-03-13 22:05:58 +0800

    update record

--- src/apis/record/getRecord.js
+++ src/apis/record/getRecord.js
@@ -1,19 +1,20 @@
-import models from '../../../models';
-
+import models from "../../../models";
 
 export default function getRecord(req, res, next) {
-    models.Record
-        .findOne({
-            where:{
-                id: req.params.id
-            },
-            include: [
-                { model: models.Doctor },
-                { model: models.Patient },
-                { model: models.RecordFactor, include: models.Factor },
-                { model: models.RecordMedicine, include: models.Medicine }
-            ]
-        }).then(records=>{
-            res.json(records);
-        })
+  models.Record.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: models.Doctor,
+        include: [{ model: models.District }, { model: models.Specialty }]
+      },
+      { model: models.Patient },
+      { model: models.RecordFactor, include: models.Factor },
+      { model: models.RecordMedicine, include: models.Medicine }
+    ]
+  }).then(records => {
+    res.json(records);
+  });
 }
