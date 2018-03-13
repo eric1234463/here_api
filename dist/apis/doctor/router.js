fe8f5c6e5c200bb918a5a6bfcc9a801c008d9ece
eric1234463 <eric1234463@gmail.com>
2018-03-13 22:13:55 +0800

    add getDoctors

--- dist/apis/doctor/router.js
+++ dist/apis/doctor/router.js
@@ -16,6 +16,10 @@ var _getDoctor = require("./getDoctor");
 
 var _getDoctor2 = _interopRequireDefault(_getDoctor);
 
+var _getDoctors = require("./getDoctors");
+
+var _getDoctors2 = _interopRequireDefault(_getDoctors);
+
 var _updateDoctor = require("./updateDoctor");
 
 var _updateDoctor2 = _interopRequireDefault(_updateDoctor);
@@ -25,6 +29,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var doctorRouter = _express2.default.Router();
 doctorRouter.post("/login", _doctorLogin2.default);
 doctorRouter.get("/location", _doctorLocation2.default);
+doctorRouter.get("/", _getDoctors2.default);
 doctorRouter.get("/:id", _getDoctor2.default);
 doctorRouter.put("/:id", _updateDoctor2.default);
 
--- src/apis/doctor/router.js
+++ src/apis/doctor/router.js
@@ -2,11 +2,13 @@ import express from "express";
 import doctorLogin from "./doctorLogin";
 import doctorLocation from "./doctorLocation";
 import getDoctor from "./getDoctor";
+import getDoctors from "./getDoctors";
 import updateDoctor from "./updateDoctor";
 
 const doctorRouter = express.Router();
 doctorRouter.post("/login", doctorLogin);
 doctorRouter.get("/location", doctorLocation);
+doctorRouter.get("/", getDoctors);
 doctorRouter.get("/:id", getDoctor);
 doctorRouter.put("/:id", updateDoctor);
 
