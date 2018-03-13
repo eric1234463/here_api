e30500928ea5294ebb5f2e0d94b94d4ec0ac08bf
eric1234463 <eric1234463@gmail.com>
2018-03-13 22:08:34 +0800

    update getRecords

--- dist/apis/record/getRecords.js
+++ dist/apis/record/getRecords.js
@@ -1,24 +1,27 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = getRecords;
 
-var _models = require('../../../models');
+var _models = require("../../../models");
 
 var _models2 = _interopRequireDefault(_models);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function getRecords(req, res, next) {
-    _models2.default.Record.findAll({
-        where: {
-            patientId: req.query.userId
-        },
-        include: [{ model: _models2.default.Doctor }, { model: _models2.default.Patient }, { model: _models2.default.RecordFactor, include: _models2.default.Factor }, { model: _models2.default.RecordMedicine, include: _models2.default.Medicine }]
-    }).then(function (records) {
-        res.json(records);
-    });
+  _models2.default.Record.findAll({
+    where: {
+      patientId: req.query.userId
+    },
+    include: [{
+      model: _models2.default.Doctor,
+      include: [{ model: _models2.default.District }, { model: _models2.default.Specialty }]
+    }, { model: _models2.default.Patient }, { model: _models2.default.RecordFactor, include: _models2.default.Factor }, { model: _models2.default.RecordMedicine, include: _models2.default.Medicine }]
+  }).then(function (records) {
+    res.json(records);
+  });
 }
 //# sourceMappingURL=getRecords.js.map
--- src/apis/record/getRecords.js
+++ src/apis/record/getRecords.js
@@ -1,19 +1,20 @@
-import models from '../../../models';
-
+import models from "../../../models";
 
 export default function getRecords(req, res, next) {
-    models.Record
-        .findAll({
-            where: {
-                patientId: req.query.userId
-            },
-            include: [
-                { model: models.Doctor },
-                { model: models.Patient },
-                { model: models.RecordFactor, include: models.Factor },
-                { model: models.RecordMedicine, include: models.Medicine }
-            ]
-        }).then(records => {
-            res.json(records);
-        })
+  models.Record.findAll({
+    where: {
+      patientId: req.query.userId
+    },
+    include: [
+      {
+        model: models.Doctor,
+        include: [{ model: models.District }, { model: models.Specialty }]
+      },
+      { model: models.Patient },
+      { model: models.RecordFactor, include: models.Factor },
+      { model: models.RecordMedicine, include: models.Medicine }
+    ]
+  }).then(records => {
+    res.json(records);
+  });
 }
